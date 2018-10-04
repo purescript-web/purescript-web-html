c33992520b1c2749e77c519f0e3d4bde36a7d438
Elliot Davies <elliot.a.davies@gmail.com>
2018-10-04 16:18:17 +0100

    Add support for navigator language and userAgent

--- src/Web/HTML/Navigator.purs
+++ src/Web/HTML/Navigator.purs
@@ -4,4 +4,8 @@ import Effect (Effect)
 
 foreign import data Navigator :: Type
 
+foreign import language :: Navigator -> Effect String
+
 foreign import platform :: Navigator -> Effect String
+
+foreign import userAgent :: Navigator -> Effect String
