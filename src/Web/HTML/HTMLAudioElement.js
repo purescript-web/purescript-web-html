ef295dbde6f59f47b8777668e38915a173eccc8e
toastal <toastal@protonmail.com>
2020-12-12 03:09:31 +0000

    create new Audio (#32)

---
+++ src/Web/HTML/HTMLAudioElement.js
@@ -0,0 +1,14 @@
+/* global Audio */
+"use strict";
+
+exports.create = function () {
+  return function () {
+    return new Audio();
+  };
+};
+
+exports.createWithURL = function (url) {
+  return function () {
+    return new Audio(url);
+  };
+};
